79a50e5bd974de04f00d79037cfc18a7832f0e81
v.kragel <v.kragel@softteco.com>
2025-07-04 14:31:39 +0300

    refactor: update template icon

--- client/src/shared/model/icons.ts
+++ client/src/shared/model/icons.ts
@@ -1,8 +1,8 @@
-import { BookDashed, Cuboid, House, User } from "lucide-react";
+import { Blocks, Cuboid, House, User } from "lucide-react";
 
 export const iconsMap = {
   UserIcon: User,
   HomeIcon: House,
   FieldIcon: Cuboid,
-  TemplateIcon: BookDashed,
+  TemplateIcon: Blocks,
 };
