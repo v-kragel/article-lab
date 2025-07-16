7872a9c80f87ac42b94a8c4a7124290b50852e95
v.kragel <v.kragel@softteco.com>
2025-07-16 15:50:38 +0300

    feat: add response wrapper module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from "./app.service";
 import { RequestContextModule } from "./common/context";
 import { ExceptionModule } from "./common/exceptions";
 import { LoggerModule } from "./common/logger";
+import { ResponseWrapperModule } from "./common/response-wrapper";
 import { appConfig, databaseConfig } from "./config";
 import { FieldModule } from "./modules/field";
 import { TemplateModule } from "./modules/template";
@@ -12,15 +13,23 @@ import { PrismaModule } from "./prisma";
 
 @Module({
   imports: [
+    // Core NestJS config
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ".env",
       load: [databaseConfig, appConfig],
     }),
+
+    // Global infrastructure (middleware / logging / error handling)
     LoggerModule,
     ExceptionModule,
     RequestContextModule,
+    ResponseWrapperModule,
+
+    // DB
     PrismaModule,
+
+    // Feature modules (business logic)
     FieldModule,
     TemplateModule,
   ],
---
+++ server/src/common/response-wrapper/index.ts
@@ -0,0 +1,2 @@
+export * from "./dto/response-wrapper.dto";
+export * from "./response-wrapper.module";
